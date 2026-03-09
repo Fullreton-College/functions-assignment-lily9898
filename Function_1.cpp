6044aeb697ccbe6c0242f0ceb4fd5fe562b98e6f
Kim, Lily <choiheewoo@gmail.com>
2026-03-09 10:10:53 -0700

    Code changes complete

--- Function_1.cpp
+++ Function_1.cpp
@@ -2,14 +2,38 @@
 #include <cstdlib>
 using namespace std;
 
-/* Define your function here */ 
+/* Define your function here */
+string CoinFlip()
+{
+   int randomValue = rand() % 2;
 
-int main() {
+   if (randomValue == 1)
+   {
+      return "Heads";
+   }
+
+   else
+   {
+      return "Tails";
+   }
+}
+
+int main()
+{
    // Add more variables as needed
-   
+   int TimesCoinFlip;
+
    srand(2);  // Unique seed
 
    /* Type your code here */
+   cout << "Enter the desired number of coin flips: ";
+   cin >> TimesCoinFlip;
+
+   for (int i = 0; i < TimesCoinFlip; i ++)
+   {
+      cout << CoinFlip() << " ";
+   }
+   cout << endl;
 
    return 0;
 }
